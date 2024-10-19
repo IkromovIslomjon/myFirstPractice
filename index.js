b8c8da1ddf46370546acca579c2c65fdfbca2795
IkromovIslomjon <167564180+IkromovIslomjon@users.noreply.github.com>
2024-10-19 09:50:50 -0400

    firstCommit

---
+++ index.js
@@ -0,0 +1,48 @@
+window.addEventListener('DOMContentLoaded', function(){
+    let products=document.querySelectorAll('.product')
+    let buttons=document.querySelectorAll('button')
+    let openBtn=document.querySelector('.open')
+
+function createCart(){
+    let cart=document.createElement('div')
+    let field=document.createElement('div')
+    let heading=document.createElement('h2')
+    let closeBtn=document.createElement('button')
+
+
+    cart.classList.add('cart')
+    field.classList.add('cart-field')
+    closeBtn.classList.add('close')
+
+    heading.textContent='Your purshases'
+    closeBtn.textContent='Close'
+
+    document.body.appendChild(cart)
+    cart.appendChild(heading)
+    cart.appendChild(field)
+    cart.appendChild(closeBtn)
+}
+createCart();
+
+let cart=document.querySelector('.cart')
+let closeBtn=document.querySelector('.close')
+let field=document.querySelector('.cart-field')
+openBtn.addEventListener('click', function(){
+    cart.style.display='block'
+})
+closeBtn.addEventListener('click', function(){
+    cart.style.display='none'
+
+})
+buttons.forEach(function(item, i){
+    item.addEventListener('click', function(){
+        let cloneItem=products[i].cloneNode(true)
+        let btn=cloneItem.querySelector('button')
+
+        btn.remove();
+        field.appendChild(cloneItem);
+        products[i].remove();
+    })
+})
+
+})
